5d42aa670f2f88fc33a3246b99aaa5ed90ca7d1d
LamaKhaledd <s12027995@stu.najah.edu>
2025-08-03 17:52:12 +0300

    Add stateful handlers for counting homepage requests, metrics and reset endpoints

--- main.go
+++ main.go
@@ -1,22 +1,47 @@
 package main
 
 import (
-    "net/http"
+	"fmt"
+	"net/http"
+	"sync/atomic"
 )
 
-func readinessHandler(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-    w.WriteHeader(http.StatusOK)
-    w.Write([]byte("OK"))
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	count := cfg.fileserverHits.Load()
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hits: %d", count)
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits counter reset"))
 }
 
 func main() {
-    mux := http.NewServeMux()
+	apiCfg := &apiConfig{}
 
-    mux.HandleFunc("/healthz", readinessHandler)
+	mux := http.NewServeMux()
 
-    fs := http.FileServer(http.Dir("app"))
-    mux.Handle("/app/", http.StripPrefix("/app", fs))
+	fileServer := http.FileServer(http.Dir("app"))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", fileServer)))
 
-    http.ListenAndServe(":8080", mux)
+	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
+
+	fmt.Println("Server running on http://localhost:8080")
+	http.ListenAndServe(":8080", mux)
 }
